d0b95b0b1db717e3c8aae26a8566540a319f59cf
kent <kent2980@gmail.com>
2018-05-04 00:58:59 +0900

    動作安定版

--- JockeysLink/src/com/servlet/RaceSouceServlet.java
+++ JockeysLink/src/com/servlet/RaceSouceServlet.java
@@ -3,7 +3,7 @@ package com.servlet;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
- 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,7 +21,7 @@ import com.pckeiba.umagoto.UmagotoDataSet;
 @WebServlet("/Race")
 public class RaceSouceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,16 +34,16 @@ public class RaceSouceServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	       
+
 		//URLパラメータからレースコードを取得する
 		String requestPara = request.getParameter("racecode");
-		  
+
 		//各レース詳細オブジェクトを取得する
 		RaceDataLoader loader = new RaceDataLoader(requestPara,4);
 		RaceDataSet raceData = loader.getRaceDataSet();
 		List<UmagotoDataSet> umaList = loader.getNowRaceDataList();
 		List<Map<Integer,UmagotoDataSet>> umaMap = loader.getKakoRaceDataMapList();
-		
+
 		//各レース詳細オブジェクトをフォワードする
 		request.setAttribute("raceData", raceData);
 		request.setAttribute("umaList", umaList);
